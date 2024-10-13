ec5e35489242c5558b1cb18b008419523c7615c9
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-13 20:44:01 +0300

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-      resolve(true);
-    } else {
-      reject(Error, 'request failed');
-    }
+export default function getResponseFromAPI() {
+  return new Promise(() => {
   });
 }
-
-getResponseFromAPI()
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
