eef6266d17f7f85b05ac59e457dfa0bcb6295b7a
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-15 15:38:19 +0300

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import uploadPhoto from './utils.js';
+import createUser from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const [photo, user] = values;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
